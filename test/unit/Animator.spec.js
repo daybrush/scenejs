98d42cce86027d65873de9ae036797de7e07f144
Daybrush <itoolsg@gmail.com>
2018-04-12 11:27:24 +0900

    add print-coverage

---
+++ test/unit/Animator.spec.js
@@ -0,0 +1,12 @@
+import Animator from "../../src/Animator";
+/* eslint-disable */
+
+
+describe("Animator Test", function() {
+	describe("test Animator initialize", function() {
+		it ("should check Animator initialize", function() {
+			const animator = new Animator();
+
+		});
+	});
+});
