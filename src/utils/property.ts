e367a3738d860b5e14aed333c67dbec95e3afaa9
Daybrush <daybrush@gmail.com>
2020-04-29 23:17:15 +0900

    chore: Release 1.1.6

--- src/utils/property.ts
+++ src/utils/property.ts
@@ -5,27 +5,27 @@
 
 import PropertyObject from "../PropertyObject";
 import {
-  COLOR_MODELS, isString,
-  splitComma, splitSpace, stringToRGBA,
-  RGBA, splitBracket, IObject, isArray
+    COLOR_MODELS, isString,
+    splitComma, splitSpace, stringToRGBA,
+    RGBA, splitBracket, IObject, isArray
 } from "@daybrush/utils";
 import { NameType } from "../types";
 
 export function splitStyle(str: string) {
-  const properties = str.split(";");
-  const obj: IObject<string | PropertyObject> = {};
-  let length = properties.length;
+    const properties = str.split(";");
+    const obj: IObject<string | PropertyObject> = {};
+    let length = properties.length;
 
-  for (let i = 0; i < length; ++i) {
-    const matches = /([^:]*):([\S\s]*)/g.exec(properties[i]);
+    for (let i = 0; i < length; ++i) {
+        const matches = /([^:]*):([\S\s]*)/g.exec(properties[i]);
 
-    if (!matches || matches.length < 3 || !matches[1]) {
-      --length;
-      continue;
+        if (!matches || matches.length < 3 || !matches[1]) {
+            --length;
+            continue;
+        }
+        obj[matches[1].trim()] = toPropertyObject(matches[2].trim());
     }
-    obj[matches[1].trim()] = toPropertyObject(matches[2].trim());
-  }
-  return {styles: obj, length};
+    return { styles: obj, length };
 }
 /**
 * convert array to PropertyObject[type=color].
@@ -39,18 +39,18 @@ arrayToColorObject([0, 0, 0])
 // => PropertyObject(type="color", model="rgba", value=[0, 0, 0, 1], separator=",")
 */
 export function arrayToColorObject(arr: number[]) {
-  const model = RGBA;
+    const model = RGBA;
 
-  if (arr.length === 3) {
-    arr[3] = 1;
-  }
-  return new PropertyObject(arr, {
-    model,
-    separator: ",",
-    type: "color",
-    prefix: `${model}(`,
-    suffix: ")",
-  });
+    if (arr.length === 3) {
+        arr[3] = 1;
+    }
+    return new PropertyObject(arr, {
+        model,
+        separator: ",",
+        type: "color",
+        prefix: `${model}(`,
+        suffix: ")",
+    });
 }
 /**
 * convert text with parentheses to object.
@@ -63,42 +63,42 @@ stringToBracketObject("abcde(0, 0, 0,1)")
 // => PropertyObject(model="abcde", value=[0, 0, 0,1], separator=",")
 */
 export function stringToBracketObject(text: string) {
-  // [prefix, value, other]
-  const { prefix: model, value, suffix: afterModel } = splitBracket(text);
+    // [prefix, value, other]
+    const { prefix: model, value, suffix: afterModel } = splitBracket(text);
 
-  if (typeof value === "undefined") {
-    return text;
-  }
-  if (COLOR_MODELS.indexOf(model) > -1) {
-    return arrayToColorObject(stringToRGBA(text));
-  }
-  // divide comma(,)
-  const obj = toPropertyObject(value, model);
+    if (typeof value === "undefined") {
+        return text;
+    }
+    if (COLOR_MODELS.indexOf(model) > -1) {
+        return arrayToColorObject(stringToRGBA(text));
+    }
+    // divide comma(,)
+    const obj = toPropertyObject(value, model);
 
-  let arr = [value];
-  let separator = ",";
-  let prefix = `${model}(`;
-  let suffix = `)${afterModel}`;
+    let arr = [value];
+    let separator = ",";
+    let prefix = `${model}(`;
+    let suffix = `)${afterModel}`;
 
-  if (obj instanceof PropertyObject) {
-    separator = obj.separator;
-    arr = obj.value;
-    prefix += obj.prefix;
-    suffix = obj.suffix + suffix;
-  }
-  return new PropertyObject(arr, {
-    separator,
-    model,
-    prefix,
-    suffix,
-  });
+    if (obj instanceof PropertyObject) {
+        separator = obj.separator;
+        arr = obj.value;
+        prefix += obj.prefix;
+        suffix = obj.suffix + suffix;
+    }
+    return new PropertyObject(arr, {
+        separator,
+        model,
+        prefix,
+        suffix,
+    });
 }
 
 export function arrayToPropertyObject(arr: any[], separator: string) {
-  return new PropertyObject(arr, {
-    type: "array",
-    separator,
-  });
+    return new PropertyObject(arr, {
+        type: "array",
+        separator,
+    });
 }
 
 /**
@@ -113,9 +113,9 @@ stringToColorObject("rgba(0, 0, 0,1)")
 // => PropertyObject(type="color", model="rgba", value=[0, 0, 0,1], separator=",")
 */
 export function stringToColorObject(value: string): string | PropertyObject {
-  const result = stringToRGBA(value);
+    const result = stringToRGBA(value);
 
-  return result ? arrayToColorObject(result) : value;
+    return result ? arrayToColorObject(result) : value;
 }
 /**
 * convert CSS Value to PropertyObject
@@ -133,54 +133,54 @@ export function toPropertyObject(value: any[], model?: NameType): PropertyObject
 export function toPropertyObject(value: IObject<any>, model?: NameType): IObject<any>;
 export function toPropertyObject(value: string, model?: NameType): PropertyObject | string;
 export function toPropertyObject(value: string | IObject<any> | any[], model?: NameType) {
-  if (!isString(value)) {
-    if (isArray(value)) {
-      return arrayToPropertyObject(value, ",");
+    if (!isString(value)) {
+        if (isArray(value)) {
+            return arrayToPropertyObject(value, ",");
+        }
+        return value;
+    }
+    let values: any = splitComma(value);
+
+    if (values.length > 1) {
+        return arrayToPropertyObject(values.map(v => toPropertyObject(v)), ",");
+    }
+    values = splitSpace(value);
+
+    if (values.length > 1) {
+        return arrayToPropertyObject(values.map(v => toPropertyObject(v)), " ");
+    }
+    values = /^(['"])([^'"]*)(['"])$/g.exec(value);
+
+    if (values && values[1] === values[3]) {
+        // Quotes
+        return new PropertyObject([toPropertyObject(values[2])], {
+            prefix: values[1],
+            suffix: values[1],
+        });
+    } else if (value.indexOf("(") !== -1) {
+        // color
+        return stringToBracketObject(value);
+    } else if (value.charAt(0) === "#" && model !== "url") {
+        return stringToColorObject(value);
     }
     return value;
-  }
-  let values = splitComma(value);
-
-  if (values.length > 1) {
-    return arrayToPropertyObject(values.map(v => toPropertyObject(v)), ",");
-  }
-  values = splitSpace(value);
-
-  if (values.length > 1) {
-    return arrayToPropertyObject(values.map(v => toPropertyObject(v)), " ");
-  }
-  values = /^(['"])([^'"]*)(['"])$/g.exec(value);
-
-  if (values && values[1] === values[3]) {
-    // Quotes
-    return new PropertyObject([toPropertyObject(values[2])], {
-      prefix: values[1],
-      suffix: values[1],
-    });
-  } else if (value.indexOf("(") !== -1) {
-    // color
-    return stringToBracketObject(value);
-  } else if (value.charAt(0) === "#" && model !== "url") {
-    return stringToColorObject(value);
-  }
-  return value;
 }
 export function toObject(object: PropertyObject, result: IObject<any> = {}) {
-  const model = object.model;
+    const model = object.model;
 
-  if (model) {
-    object.setOptions({
-      model: "",
-      suffix: "",
-      prefix: "",
-    });
-    const value = object.size() > 1 ? object : object.get(0);
+    if (model) {
+        object.setOptions({
+            model: "",
+            suffix: "",
+            prefix: "",
+        });
+        const value = object.size() > 1 ? object : object.get(0);
 
-    result[model] = value;
-  } else {
-    object.forEach(obj => {
-      toObject(obj, result);
-    });
-  }
-  return result;
+        result[model] = value;
+    } else {
+        object.forEach(obj => {
+            toObject(obj, result);
+        });
+    }
+    return result;
 }
